f9708c917106a7aebeed731bb30977fd55b8b2ca
Viktoria J <jurcsak.viki@gmail.com>
2021-07-15 20:54:07 +0200

    Create 01-most_commits.py

---
+++ 11-Tuples/01-most_commits.py
@@ -0,0 +1,28 @@
+# Exercise 1: Revise a previous program as follows: Read and parse the “From” lines and pull out the addresses from the line.
+# Count the number of messages from each person using a dictionary.
+# After all the data has been read, print the person with the most commits by creating a list of (count, email) tuples from the dictionary. 
+# Then sort the list in reverse order and print out the person who has the most commits.
+
+file = open("mbox-short.txt")
+count = dict()
+for line in file:
+    words = line.split()
+    
+    # guardian in a compound statement
+    if len(words) < 3 or words[0] != "From":
+        continue
+    else:
+        words = words[1]
+    count[words] = count.get(words, 0) + 1
+
+#lst = []
+#for key, val in count.items():
+#    lst.append((val, key))
+
+#for key, val in sorted(lst, reverse = True):
+#    print(val, key)
+
+
+# this line of code does almost the exact same thing as the upper one 
+
+print(sorted([(v, k) for k, v in count.items()], reverse = True))
