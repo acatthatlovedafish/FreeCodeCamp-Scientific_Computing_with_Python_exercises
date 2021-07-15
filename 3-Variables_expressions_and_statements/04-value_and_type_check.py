189bd61124c6e99792aac518e507320600504da9
Viktoria J <jurcsak.viki@gmail.com>
2021-07-15 19:20:55 +0200

    Create 04-value_and_type_check.py

---
+++ 3-Variables_expressions_and_statements/04-value_and_type_check.py
@@ -0,0 +1,13 @@
+# Exercise 4: Assume that we execute the following assignment statements:
+width = 17
+height = 12.0
+# For each of the following expressions, write the value of the expression and the type (of the value of the expression).
+
+#1. 8, int
+print(width//2)
+#2. 8,5 float
+print(width/2.0)
+#3. 4, float
+print(height/3)
+#4. 11, int
+print(1 + 2 * 5)
