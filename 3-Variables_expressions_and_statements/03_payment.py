393c299c99ac6f9e47399c80402425619d5ef6e5
Viktoria J <jurcsak.viki@gmail.com>
2021-07-15 19:03:38 +0200

    Update 03_payment.py

--- 3-Variables_expressions_and_statements/03_payment.py
+++ 3-Variables_expressions_and_statements/03_payment.py
@@ -1,3 +1,4 @@
+# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
 hours = input("Enter the hours: ")
 money = input("Enter the rate: ")
 payment =  int(hours) * float(money)
