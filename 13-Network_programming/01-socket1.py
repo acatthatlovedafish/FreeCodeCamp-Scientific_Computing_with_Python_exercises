e85432a19f04effe362162e2877dd326d69b812c
Viktoria J <jurcsak.viki@gmail.com>
2021-07-17 13:45:10 +0200

    Update 01-socket1.py

--- 13-Network_programming/01-socket1.py
+++ 13-Network_programming/01-socket1.py
@@ -12,12 +12,12 @@ try:
     mysock.connect((host_name, 80))
     cmd = f"GET {url} HTTP/1.0\r\n\r\n".encode()
     mysock.send(cmd)
-
-    while True:
-        data = mysock.recv(512)
-        if len(data) < 1:
-            break
-        print(data.decode(), end = "")
-    mysock.close()
 except:
     print("Please enter a valid web address without www-s.")
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end = "")
+mysock.close()
