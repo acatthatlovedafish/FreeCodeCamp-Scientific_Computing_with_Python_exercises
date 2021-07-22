ad1a3175833b55f4cbe257222897b459cc9a6b41
Viktoria J <jurcsak.viki@gmail.com>
2021-07-22 23:26:29 +0200

    Create 01-class.py

---
+++ 15-Object-oriented_programming/01-class.py
@@ -0,0 +1,14 @@
+class PartyAnimal:
+    x = 0
+
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+an = PartyAnimal()
+
+print("Type:", type(an))
+print("Dir:", dir(an))
+an.party()
+an.party()
+an.party()
